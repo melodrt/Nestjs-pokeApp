887fa69bfa8be5a036ed242bd1c32983b75ed486
meloduarte <devscustomers@gmail.com>
2025-11-12 21:15:01 -0500

    recibir y validar la data

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -3,7 +3,7 @@ import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
-@Controller('api/v2/pokemon')
+@Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
